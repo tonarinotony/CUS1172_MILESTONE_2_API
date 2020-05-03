cbe5aba001f1dacd7b55b4b779d61ca94567a330
tonarinotony <58190955+tonarinotony@users.noreply.github.com>
2020-05-03 17:21:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ app.use('/quiz', quiz);
 app.get('/', function(req,res) { 
 	res.send('<h1> Hello Tony Quiz API </h1>')
 })
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){
+	console.log('Example app listening on port 3000')
+});
