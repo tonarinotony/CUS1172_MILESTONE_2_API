3dcf60d6688b9631042ee50baaab2bc4d05ac5f6
tonarinotony <58190955+tonarinotony@users.noreply.github.com>
2020-05-03 00:05:19 -0400

    quizIDworking
    get api by quiz is working now

--- quiz.js
+++ quiz.js
@@ -1,13 +1,16 @@
 var express = require('express');
 const data = require('./quiz_dataset')
 const router = express.Router()
+const quiz_data = data.quiz
 
 router.get('/list', (req,res) => {
 	res.json(data.list)
 });
 
 router.get('/:quizid', (req,res) => {
-	res.json(data.quiz)
+	var q_id = req.params['quizid'];
+	quiz_with_id = quiz_data["results"].filter(q => q.quizId == q_id)
+	res.json(quiz_with_id)
 });
 
 router.get('/:quizid/:questionid', (req,res) => {
@@ -15,7 +18,7 @@ router.get('/:quizid/:questionid', (req,res) => {
 });
 
 router.get('/check_answer/:quizid/:questionid/:answer', (req,res) => {
-	res.send('<h2>questionid ID</h2>');
+	res.send('<h2>answer ID</h2>');
 });
 
 module.exports = router
--- quiz_dataset.js
+++ quiz_dataset.js
@@ -1,30 +1,73 @@
 const list = {
 	list:
 	[{
-		"quizid": "1",
+		"quizId": "1",
 		"title" : "quiz 1",
 		"description" : "One of two available Quizzes"
 	},
 	{
-		"quizid": "2",
+		"quizId": "2",
 		"title" : "quiz 2",
 		"description" : "One of two available Quizzes"
 	}
 	]
 }
 const quiz = {
-	results: [
+	results: [{
+		"quizId": 1,
+		"questions":[
+			{
+				"data":{
+				"id": 100,
+				"question_type" : "mc",
+				"question" : "What is 1+1",
+				"answer_options" : ["1","2","3","4"]
+				},
+				"meta" :{
+					"next_question": 101
+				}
+			},
+			{
+				"data":{
+				"id": 101,
+				"question_type" : "mc",
+				"question" : "What is 1+1",
+				"answer_options" : ["1","2","3","5"]
+				},
+				"meta" :{
+					"next_question": 102
+				}
+			}
+		]
+	},
 	{
-		"data":{
-		"id": 100,
-		"question_type" : "mc",
-		"question" : "What is 1+1",
-		"answer_options" : ["1","2","3","4"]
-		},
-		"meta" :{
-			"next_question": 101
-		}
+		"quizId": 2,
+		"questions":[
+			{
+				"data":{
+				"id": 200,
+				"question_type" : "mc",
+				"question" : "What is 1+1",
+				"answer_options" : ["1","2","3","4"]
+				},
+				"meta" :{
+					"next_question": 201
+				}
+			},
+			{
+				"data":{
+				"id": 201,
+				"question_type" : "mc",
+				"question" : "What is 1+1",
+				"answer_options" : ["1","2","3","5"]
+				},
+				"meta" :{
+					"next_question": 202
+				}
+			}
+		]
 	}
+
 	]
 }
 
