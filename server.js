496de257d08d60b34146b3a80aa8ccc17b4baab6
tonarinotony <58190955+tonarinotony@users.noreply.github.com>
2020-05-04 18:16:15 -0400

    CORSAttempt2

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var quiz = require('./quiz')
 var app = express();
+const cors = require("cors") 
+app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/quiz', quiz);
 
-const cors = require("cors") 
-app.use(cors());
+
 
 app.get('/', function(req,res) { 
 	res.send('<h1> Hello Tony Quiz API </h1>')
